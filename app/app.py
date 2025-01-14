ea781d4f82578fb9fe03f4e005fd155e1c230d13
manhnv <nguyenmanh0397@gmail.com>
2025-01-14 15:31:18 +0700

    edit template send otp

--- app/app.py
+++ app/app.py
@@ -121,7 +121,11 @@ def webhook():
             else:
                 user = args[1]
                 otp = get_otp(user)
-                template_msg = f"Mã OTP của {user} là:\n```txt\n{otp}\n```"
+                template_msg = f"""Mã OTP của {user} là:
+                ```txt
+                {otp}
+                ```
+                """
                 send_message(chat_id, template_msg)
         elif text.startswith("/open_time") and data["message"]["chat"]["id"] == ADMIN_ID:
             args = text.split(" ")
